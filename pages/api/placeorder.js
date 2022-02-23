405d6a88452b97ea82889d7b816ac34764753e3a
VijayM Raut <10695503@infarszb16250.lntinfotech.com>
2022-02-23 21:26:07 +0530

    order placed dynamically

--- pages/api/placeorder.js
+++ pages/api/placeorder.js
@@ -1,11 +1,33 @@
+import API_URLS from '../../api_urls'
+
 export default function handler(req, res) {
     if (req.method !== 'POST') {
         res.status(405).send({ message: 'Only POST requests allowed' })
         return
     }
-    // const body = JSON.parse(req.body)
     const body = req.body
-    console.log("body " + body.firstName)
+    const context = {"value" : body}
+    const place_order_request = getServerSideProps(context);
     res.status(200).json({status:"success"})
-    
+  }
+
+export async function getServerSideProps(context) {
+      const placeOrderUrl =API_URLS.PLACE_ORDER
+      const resp = await fetch(placeOrderUrl, {
+                    method: "POST",
+                    headers: {
+                       "Authorization": `Bearer ${process.env.ADMIN_TOKEN}`,
+                       "Content-Type": "application/json"
+                     },
+                     body: JSON.stringify(context)
+                  }
+                 )
+
+      const placeOrderIncrementId = await resp.json()
+      console.log("placeOrderIncrementId "+ JSON.stringify(placeOrderIncrementId))
+      return {
+        props: {
+          placeOrderIncrementId
+        }
+      }
   }
--- pages/billingaddress.js
+++ pages/billingaddress.js
@@ -8,12 +8,12 @@ import { useForm } from "react-hook-form";
 export default function BillingAddress(){
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
     const router = useRouter()
-    
+
     const [billingAddressState, updateBillingAddressState] = useState({firstName:"", lastName: ""})
 
     /*const handleChange = (e)=>{
         const {id, value} = e.target;
-       
+
         switch(id){
             case "firstName": updateBillingAddressState(prevState=>{return {...prevState, firstName: value}});break
             case "lastName": updateBillingAddressState(prevState=>{return {...prevState, lastName: value}});break
@@ -23,9 +23,9 @@ export default function BillingAddress(){
      }; */
 
     const onSubmit = (data) => {
-        
+
         console.log(updateBillingAddressState.firstName + " " + updateBillingAddressState.lastName)
-       
+
         data.sku = router.query.sku
         fetch('/api/placeorder', {
             method: 'POST',
@@ -49,38 +49,47 @@ export default function BillingAddress(){
                 <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="mb-3">
                    <label htmlFor="firstName" className="form-label">First Name</label>
-                   <input type="text" className="form-control" id="firstName"  {...register("firstName", { required: true, maxLength: 20 })}/>
-                   <p>{errors.firstName?.type === 'required' && "First name is required"}</p>
-                </div>  
+                   <input type="text" className="form-control" id="firstname"  {...register("address[firstname]", { required: true, maxLength: 20 })}/>
+                   <p>{errors.firstname?.type === 'required' && "First name is required"}</p>
+                </div>
                 <div className="mb-3">
                    <label htmlFor="lastName" className="form-label">Last Name</label>
-                   <input type="text" className="form-control" id="lastName"  {...register("lastName", { required: true, maxLength: 20 })}/>
-                   <p>{errors.lastName?.type === 'required' && "Last name is required"}</p>
-                </div>    
+                   <input type="text" className="form-control" id="lastname"  {...register("address[lastname]", { required: true, maxLength: 20 })}/>
+                   <p>{errors.lastname?.type === 'required' && "Last name is required"}</p>
+                </div>
                 <div className="mb-3">
                     <label htmlFor="email" className="form-label">Email address</label>
-                    <input type="email" className="form-control" id="email"  {...register("email")}/>               
+                    <input type="email" className="form-control" id="email"  {...register("email")}/>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="pwd" className="form-label">Street</label>
-                    <input type="text" className="form-control" id="pwd" {...register("street")}/>
-                </div>                
+                    <input type="text" className="form-control" id="pwd" {...register("address[street]")}/>
+                </div>
                 <div className="mb-3">
                     <label htmlFor="pwd" className="form-label">City</label>
-                    <input type="text" className="form-control" id="pwd" {...register("city")}/>
+                    <input type="text" className="form-control" id="pwd" {...register("address[city]")}/>
                 </div>
-
                 <div className="mb-3">
-                   <label htmlFor="country" className="form-label">Country</label>
-                   <select className="form-control" {...register("country")}>
+                    <label htmlFor="pwd" className="form-label">Post Code</label>
+                    <input type="text" className="form-control" id="pwd" {...register("address[postcode]")}/>
+                </div>
+                <div className="mb-3">
+                   <label htmlFor="country_id" className="form-label">Country</label>
+                   <select className="form-control" {...register("address[country_id]")}>
                         <option value="-1">--Select--</option>
-                        <option value="India">India</option>
+                        <option value="IN">India</option>
                         <option value="UAE">UAE</option>
                         <option value="KSA">KSA</option>
                    </select>
-                </div>   
-                                
-    
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="pwd" className="form-label">State / Province</label>
+                    <input type="text" className="form-control" id="pwd" {...register("address[region_id]")}/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="pwd" className="form-label">Mobile / Phone</label>
+                    <input type="text" className="form-control" id="pwd" {...register("address[telephone]")}/>
+                </div>
                 <button type="submit" className="btn btn-primary">Buy</button>
                 </form>
             </div>
