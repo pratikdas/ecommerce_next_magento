05e4f53cd33f526368a760cc3e1c0871da2f5f9f
Vijay <vijayraut2591@gmail.com>
2022-03-09 12:12:01 +0530

    enhance category page

--- pages/categories/[categoryId]/products.js
+++ pages/categories/[categoryId]/products.js
@@ -1,39 +1,55 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import API_URLS from '../../../api_urls'
+import Cookies from 'js-cookie'
+import styles from '../../../styles/Product.module.css';
 
 const Products = ({products}) => {
   const router = useRouter()
   const buy = (e)=>{
     const sku = e.target.value
-    router.push({
-      pathname: '/billingaddress',
-      query: { sku: sku }
-    })
+    const email = Cookies.get('userlogin');
+    console.log(email);
+    console.log(sku);
+    if (email) {
+      router.push({
+        pathname: '/billingaddress',
+        query: { sku: sku }
+      })
+    } else {
+      router.push({
+        pathname: '/registeruser',
+      })
+    }
   }
   if (products) {
       console.log(products.items);
       return (
       <section>
-            <div className="container">
-                <div className="row align-items-center">
-                  <ul>
-                 {
-                     products.items.map((product)=>(
-                         <li key={product.sku}>
-                         <Link href={{
-                              pathname: '/products/[sku]',
-                              query: { sku: product.sku }
-                            }}>
-                              <a>{product.name}</a>
-                              </Link> &nbsp;&nbsp;
-                              <button type="button" className="btn btn-primary" value={product.sku} onClick={buy}>Buy</button>
-                             </li>
-                         ))
-                  }
-                 </ul>
+      <div className="container">
+          <h3>Product Listing</h3>
+            <div className={styles.listingsection}>
+            {
+               products.items.map((product)=>(
+                <div className={styles.product}>
+                    <div class={styles.imagebox}>
+                        {Object.entries(product.media_gallery_entries).length !== 0  && <img class={styles.images} src={process.env.MEDIA_URL + "/catalog/product" + product.media_gallery_entries[0].file} />}
+                    </div>
+                    <div class={styles.textbox}>
+                        <h3 key={product.sku}>
+                           <Link href={{
+                                pathname: '/products/[sku]',
+                                query: { sku: product.sku }
+                              }}>
+                                <a>{product.name}</a>
+                                </Link> &nbsp;&nbsp;
+                            <button type="button" className="btn btn-primary" value={product.sku} onClick={buy}>Buy Property</button>
+                         </h3>
+                    </div>
                 </div>
-            </div>
+              ))  }
+          </div>
+        </div>
             </section>)
   } else {
     return (
@@ -46,15 +62,7 @@ const Products = ({products}) => {
 
 export default Products
 
-export async function getStaticProps({params}) {
-  /*const requestOptions = {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ username: 'magebit', password: 'Demo123' })
-  }
-  const res = await fetch(API_URLS.ADMIN_TOKEN, requestOptions)
-  const tokenResult = await res.json()*/
-
+export async function getServerSideProps({params}) {
   const productsUrlWithParam = API_URLS.PRODUCTS + "?searchCriteria[filter_groups][0][filters][0][field]=category_id&searchCriteria[filter_groups][0][filters][0][value]=:categoryId&searchCriteria[filter_groups][0][filters][0][condition_type]=eq"
   const productsURL = productsUrlWithParam.replace(":categoryId",params.categoryId)
   const res = await fetch(productsURL, {headers: {Authorization: `Bearer ${process.env.ADMIN_TOKEN}`}})
@@ -65,30 +73,4 @@ export async function getStaticProps({params}) {
       products
     }
   }
-}
-
-
-export async function getStaticPaths() {
-  const requestOptions = {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ username: 'magebit', password: 'Demo123' })
-  }
-  const res = await fetch("https://magento2-demo.magebit.com/index.php/rest/V1/integration/admin/token", requestOptions)
-  const tokenResult = await res.json()
-  console.log(tokenResult)
-
-  const res1 = await fetch("https://magento2-demo.magebit.com/index.php/rest/V1/categories", {headers: {Authorization: "Bearer "+tokenResult}})
-  const categoriesResponse = await res1.json()
-
-  const categories = categoriesResponse.children_data
-
-  const paths = []
-  categories.map(category=>{
-    paths.push({params: {categoryId: category.id.toString()}})
-  })
-
-  paths.map(path=>console.log(path.params.categoryId))
-
-  return { paths, fallback: true }
 }
